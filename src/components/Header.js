fc3491b3d55d8060f9ed60bd5b5781708be27ea9
Lucas Tostée <lucas.tostee@gmail.com>
2020-03-19 19:33:15 +0100

    Add logic when user is connecter for header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,29 @@
 export default element => {
 	function render(element) {
 		const header = document.createElement('header');
-		header.setAttribute("class", "container-fluid mt-3");
+		header.setAttribute("class", "container mt-3");
 		header.innerHTML = `
         <div class="row">
-            <div class="col-12">
-                <h1 class="text-center display-5">News</h1>
-            </div>
+            <div class="col-9 d-flex align-items-center">
+                <h1 class="display-5">News</h1>
+			</div>
+			${
+				sessionStorage.getItem("userToken") ?
+					`<div class="col-3 d-flex align-items-center justify-content-end">
+						<small class="text-danger" id="disconnect">Disconnect</small>
+					</div>`
+				: ""
+			}
         </div>
         `;
 		element.appendChild(header);
+
+		if (document.querySelector("#disconnect")) {
+			document.querySelector("#disconnect").addEventListener("click", () => {
+				sessionStorage.clear();
+				window.location.href = window.location.origin+"/";
+			});
+		}
 	}
 
 	return render(element)
