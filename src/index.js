83fa9c1508c5fcd9d28afc59d4372089151a7abf
Lucas Tostée <lucas.tostee@gmail.com>
2020-03-19 17:48:06 +0100

    Add secure routes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
-import Header from "./components/Header"
-import FormNews from "./components/FormNews"
+import Auth from "./components/Auth";
+import Header from "./components/Header";
+import FormNews from "./components/FormNews";
 
 (root => {
-	Header(root)
-	FormNews(root)
+	if (window.location.pathname === "/" || window.location.pathname === "/inscription") return Auth(root, window.location.pathname);
+
+	// Secure routes
+	if (sessionStorage.getItem("useToken")) {
+		if (window.location.pathname === "/news") {
+			Header(root)
+			return FormNews(root)
+		}
+	}
+
+	window.location.href = window.location.origin+"/";
 })(document.querySelector("#root"))
