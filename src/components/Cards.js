4fcbcee26e5e28a91cc7f1eb08e0c7e4158007f7
Lucas Tostée <lucas.tostee@gmail.com>
2020-03-24 19:34:32 +0100

    Update Card component

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,3 +1,6 @@
+import talkToApi from "../utils/talkToApi";
+import Snackbar from "./Snackbar";
+
 export default (element, data) => {
 	if (document.querySelector(".card-wrapper")) {
 		document.querySelector(".card-wrapper").innerHTML = "";
@@ -9,15 +12,14 @@ export default (element, data) => {
 
 		section.innerHTML = `
 			<div class="row">
-				<div class="col-12 card-wrapper"></div>
+				<div class="col-12 card-wrapper" style="display:flex;flex-wrap:wrap;justify-content:space-between;"></div>
 			</div>
 		`;
 
 		element.appendChild(section);
 		const cardwrapper = document.querySelector(".card-wrapper");
-		cardwrapper.style = "display:flex;flex-wrap:wrap;justify-content:space-between;";
 
-		data.map(article => {
+		data.map((article, index) => {
 			const div = document.createElement("div");
 			div.classList.add("card");
 			div.style = "width:32%;margin: 4% 0;";
@@ -28,10 +30,36 @@ export default (element, data) => {
 					<h5 class="card-title">${article.author}</h5>
 					<p class="card-text">${article.description}</p>
 					<a href="${article.url}" class="btn btn-primary" target="_blank">Read</a>
+					<small class="article-${index}">Bookmark</small>
 				</div>
 			`;
 
 			cardwrapper.appendChild(div);
+			document.querySelector(`.article-${index}`).addEventListener("click", function (e) {
+				talkToApi("https://newsapp.dwsapp.io/api/bookmark/", "post", {
+					useCustomRoute: true,
+					headers: {
+						"content-type": "Application/json"
+					},
+					body: {
+						id: article.source.id,
+						name: article.source.name,
+						description: article.description,
+						url: article.url,
+						category: "general",
+						language: "en",
+						country: "us",
+						token: sessionStorage.getItem("userToken")
+					}
+				})
+				.then(result => {
+					if (result.err === null) {
+						return Snackbar(false, `${article.author} ajouté aux favoris`, e.pageY);
+					}
+
+					return Snackbar(true, `${article.author} existe déja`, e.pageY);
+				});
+			});
 		});
 	}
 
